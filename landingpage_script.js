2a20e540fc89eb65a24bf4f84d2e5c451212ce19
RaleLeaf <23102648@usc.edu.ph>
2024-07-10 22:51:05 +0800

    dw

---
+++ landingpage_script.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function() {
+    const landingImage = document.querySelector('.landing-image');
+    landingImage.classList.add('focused');
+  });
+
+ // Detect request animation frame
+var scroll = window.requestAnimationFrame ||
+// IE Fallback
+function(callback){ window.setTimeout(callback, 1000/60)};
+var elementsToShow = document.querySelectorAll('.show-on-scroll'); 
+
+function loop() {
+
+Array.prototype.forEach.call(elementsToShow, function(element){
+if (isElementInViewport(element)) {
+element.classList.add('is-visible');
+} else {
+element.classList.remove('is-visible');
+}
+});
+
+scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
